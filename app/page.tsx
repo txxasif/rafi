3a9daff18bb7581532c87caf0894803c55980389
Asif <txxasif@gmail.com>
2024-07-28 03:21:15 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <AnimatedSection>
         <ImageGallery />
       </AnimatedSection>
-      {/* <QuoteListWithLamp /> */}
+      <QuoteListWithLamp />
       {/* <AnimatedSection>
         <LampContainer>
           <motion.div
